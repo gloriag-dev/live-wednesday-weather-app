e22037c856fb1f2bfc72802146ee4667e8a93c1f
Gloria Giannascoli <giannascoli.gloria@gmail.com>
2022-10-26 20:31:17 +0200

    add operators

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -11,6 +11,7 @@ export default function WeatherInfo(props) {
                 <li>
                     <FormattedDate date={props.data.date}></FormattedDate>
                 </li>
+                <li className='text-capitalize'>{props.data.description}</li>
             </ul>
             <div className='row mt-3'>
                 <div className='col-6' >
@@ -22,8 +23,8 @@ export default function WeatherInfo(props) {
 
                 <div className='col-6'>
             <ul>
-                <li>Humidity: {props.data.humidity}</li>
-                <li> Wind: {props.data.wind}</li>
+                <li>Humidity: {props.data.humidity}%</li>
+                <li> Wind: {props.data.wind}km/h</li>
             </ul>
                 </div>
             </div>
